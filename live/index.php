c092a7b49cd45c3b8cb2154b21aacf058ab6945a
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2022-02-20 16:34:23 -0500

    Update index.php

--- live/index.php
+++ live/index.php
@@ -1,1 +1,15 @@
-it worked
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.1/dist/alpine.min.js" defer></script>
+	<title>Production site</title>
+</head>
+<body class="bg-gray-500">
+<div clas="text-gray-900 text-xl items-center text-center">
+Production site
+</div>
+</body>
+</html>
